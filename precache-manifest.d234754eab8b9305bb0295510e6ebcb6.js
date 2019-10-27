d4ed1c82557e66cd69e70571d55754a6a983b045
Marcus Kärrman <marcus.karrman@textalk.sel>
2019-10-27 11:45:23 +0100

    Updates

---
+++ precache-manifest.d234754eab8b9305bb0295510e6ebcb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5cbc9001c4971908b2453437529521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30929b4a6faf17a72fe4",
+    "url": "/static/css/main.e0f37523.chunk.css"
+  },
+  {
+    "revision": "2118231f9846e4ed2fcc",
+    "url": "/static/js/2.f7044eec.chunk.js"
+  },
+  {
+    "revision": "30929b4a6faf17a72fe4",
+    "url": "/static/js/main.7c560c93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
